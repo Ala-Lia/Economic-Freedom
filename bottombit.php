3f5b7a393827a18454c7789dee153cfe5730dcc3
Eulalia :) <78825077+Ala-Lia@users.noreply.github.com>
2021-11-19 10:58:33 +1300

    Topbit and bottombit files

---
+++ bottombit.php
@@ -0,0 +1,8 @@
+<!-- footer comes here-->
+<footer>
+    <div>
+        <p>
+            123 Sesame Street
+        </p>
+    </div>
+</footer>
---
+++ topbit.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <meta name="description" content="Economic freedom information about the economies of 167 countries">
+    <meta name="keywords" content="Economics, immigration, inflation, government, GDP, percentage, business, freedom">
+    <meta name="author" content="Eulalia Terra">
+    
+    <title>Home Page - Economic Freedom</title>
+    
+    <!-- link to css goes here -->
+    <link href="css/econfreedom_style.css" rel="stylesheet">
+    
+</head>
